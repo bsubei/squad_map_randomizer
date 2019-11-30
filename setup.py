73920f583d80716798556d89f1cbf117fa304228
Basheer Subei <basheersubei@gmail.com>
2019-11-30 08:00:34 -0800

    adds setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='squad_map_randomizer',
+      version='0.1.0',
+      description='A tool to generate a random map rotation for Squad servers.',
+      url='http://github.com/bsubei/squad_map_randomizer',
+      author='Basheer Subei',
+      author_email='basheersubei@gmail.com',
+      license='GPLv3',
+      scripts=['squad_map_randomizer.py'],
+      )
