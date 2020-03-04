b35b54f6683faf98612b7cee90ac30c6fcb0b814
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2020-03-04 05:44:21 -0800

    includes yml config files in setup and updates release

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='squad_map_randomizer',
-      version='0.1.1',
+      version='0.2.0',
       description='A tool to generate a random map rotation for Squad servers.',
       url='http://github.com/bsubei/squad_map_randomizer',
       author='Basheer Subei',
       author_email='basheersubei@gmail.com',
       license='GPLv3',
       scripts=['squad_map_randomizer.py'],
+      package_data={
+        '': ['configs/*.yml', 'configs/examples/*.yml'],
+        }
       )
